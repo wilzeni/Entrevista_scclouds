b1c2e43790c285526774002c9128644bb526ac4d
WilZ <142467215+wilzeni@users.noreply.github.com>
2025-07-21 18:26:16 -0300

    Update Fibonacci_recursiva.c

--- Fibonacci_recursiva.c
+++ Fibonacci_recursiva.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 int fibonacci(int x){
+    if(x < 0) return -1;
+
     if(x == 0) return 0;
     if(x == 1) return 1;
     return fibonacci(x - 1) + fibonacci(x - 2);
@@ -15,5 +17,6 @@ int main()
     scanf("%d", &n);
 
     if(fibonacci(n) > 0) printf("Numero pra seguencia de fib(%d) eh %d\n", n, fibonacci(n));
+    else printf("Numero n invalido!");
     return 0;
 }
